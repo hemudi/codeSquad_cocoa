eba16085080ef846bef97fc50f7b62487eb562cf
Hemudi <ksum1205@naver.com>
2021-11-03 14:30:31 +0900

    [2진수게임] 문제 1번 결과 배열로 형변환 추가

--- daily_study_log/log_1103/numberGame.js
+++ daily_study_log/log_1103/numberGame.js
@@ -2,13 +2,18 @@
 
 [Mission] 2진수 돌아가며 말하기 게임
 
-문제 1) T개의 숫자까지 M명이 말하고 이를 모두 출력하는 프로그램
+문제 1) 각각 T개의 숫자를 M명이 번갈아 말하고 이를 모두 출력하는 프로그램
+    * 1개의 숫자 == 진수 변환 전 숫자 1개
 
     [1] solution(진법, T, M) 값 입력받으며 호출
     [2] maxCount = T * M
     [3] for index = 0 to maxCount
             result += index.toString(진법)
     [4] result 출력
+
+    // 배열로 바꿔서 출력
+    [4-2] result.split(""); 출력
+          => 출력 형태 ['0', '1', '1', ... , '1']
 
 */
 
@@ -20,7 +25,9 @@ function solution(baseNum, numCount, numPlayer){
         result += index.toString(baseNum);
     }
 
-    console.log(result);
+    let resultArr2 = result.split("");
+
+    console.log(resultArr2);
 }
 
 solution(2, 4, 2);
