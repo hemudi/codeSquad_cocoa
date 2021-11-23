743395e38716cc3e656ea9437197582b1fec7190
Hemudi <ksum1205@naver.com>
2021-11-23 16:10:39 +0900

    [README] detector 수정 전으로 되돌리기

--- daily_study_log/log_1122/script.js
+++ daily_study_log/log_1122/script.js
@@ -6,84 +6,93 @@
     [ ] 날짜 별 TODO 보기 기능 구현
 */
 
+/*
+    [ Class ]
+    1. DataManager : Model
+    2. ViewManager : View
+    3. Controller : Controller
+    4. TaskLiElement : 하나의 일정 Li 객체 
+
+    [추가 이벤트 과정]
+    1. 새로운 일정이 입력되면 Controller 가 해당 정보를 TaskLiElement 객체로 만들어서 View 에게 넘겨줌
+    2. View 가 그걸 화면에 그림
+
+*/
+
 class DataManager {
-    constructor(storageKey) {
+    constructor(storageKey){
         this.tempStorage = [];
-        this.storageKey = storageKey; 
+        this.storageKey = storageKey;
     }
 
-    getStoredData() {
+    getStoredData(){
         const taskData = localStorage.getItem(this.storageKey);
         this.tempStorage = (taskData !== null ? JSON.parse(taskData) : new Array());
         return this.tempStorage;
     }
 
-    setLocalStorage() {
+    setLocalStorage(){
         localStorage.setItem(this.storageKey, JSON.stringify(this.tempStorage));
     }
 
-    saveTaskData(id, contents, dueDate, checked = false) {
+    saveTaskData(id, contents, dueDate, checked = false){
         this.tempStorage.push({
-            id: id,
-            contents: contents,
-            dueDate: dueDate,
-            checked: checked
+            id : id,
+            contents : contents,
+            dueDate : dueDate,
+            checked : checked
         });
         this.setLocalStorage();
     }
 
-    removeTaskData(id) {
-        const index = this.getTempStorageIndex(id);
-        if (index >= 0) this.tempStorage.splice(index, 1);
-        this.setLocalStorage();
-    }
-
-    getTempStorageIndex(id) {
+    getTempStorageIndex(id){
         const notExists = -1;
         let index = 0;
         let getId = 0;
-        for (const task of this.tempStorage) {
+        for(const task of this.tempStorage){
             getId = task['id'];
-            if (getId === +id) {
-                return index
-            };
+            if(getId === +id) { return index };
             index++;
         }
         return notExists;
     }
 
-    toggleTaskChecked(id) {
+    removeTaskData(id){
         const index = this.getTempStorageIndex(id);
-        if (index >= 0) {
+        if(index >= 0) this.tempStorage.splice(index, 1);
+        this.setLocalStorage();
+    }
+
+    toggleTaskChecked(id){
+        const index = this.getTempStorageIndex(id);
+        if(index >= 0){
             this.tempStorage[index]['checked'] = !this.tempStorage[index]['checked'];
             this.setLocalStorage();
         }
     }
 
-    getTempStorage() {
+    getTempStorage(){
         return this.tempStorage;
     }
-
-
 }
 
 class ViewManager {
-    constructor() {
-        this.$taskList = document.querySelector('#taskList'); 
-        this.eventDetector = function () {};
+    constructor(){
+        this.$taskList = document.querySelector('#taskList');
+        this.eventDetector = function(){};
     }
 
-    setEventDetector(eventDetector) {
+    setDetector(eventDetector){
         this.eventDetector = eventDetector;
     }
 
-    createNewLi(taskId, contents, dueDate, checked = false) {
+    addTask(taskId, contents, dueDate, checked = false){
         const $removeBtn = this.createRemoveButton();
         const $checkBox = this.createCheckBox(taskId);
         const $dateLabel = this.createLabel(taskId, 'date', dueDate);
         const $taskLabel = this.createLabel(taskId, 'contents', contents);
 
-        if (checked) {
+        if(checked){
             $checkBox.checked = checked;
             this.toggleLabelClass([$taskLabel, $dateLabel]);
         }
@@ -92,7 +101,7 @@ class ViewManager {
         document.querySelector('#taskList').appendChild($taskLi);
     }
 
-    createLabel(taskId, className, value) {
+    createLabel(taskId, className, value){
         const $label = document.createElement('label');
         $label.classList.add(className);
         $label.innerText = value;
@@ -100,135 +109,122 @@ class ViewManager {
         return $label;
     }
 
-    createRemoveButton() {
+    // need test
+    toggleLabelClass(labels){
+        for(const label of labels){ label.classList.toggle('label_Checked'); }
+    }
+
+    createLi(...items){
+        const $taskLi = document.createElement('li');
+        for(const item of items){ $taskLi.append(item); }
+        return $taskLi;
+    }
+
+    createRemoveButton(){
         const $removeBtn = document.createElement('button');
         $removeBtn.classList.add('removeBtn');
         $removeBtn.innerText = '×';
-        $removeBtn.addEventListener('click', (e) => {
-            this.eventDetector('remove', e)
-        });
+        $removeBtn.addEventListener('click', (e) => {this.removeButtonEventHandler(e)});
         return $removeBtn;
     }
 
-    createCheckBox(taskId) {
+    createCheckBox(taskId){
         const $checkBox = document.createElement('input');
         $checkBox.type = 'checkbox';
         $checkBox.id = taskId;
         $checkBox.classList.add('checkBox');
-        $checkBox.addEventListener('click', (e) => {
-            this.eventDetector('check', e)
-        });
+        $checkBox.addEventListener('click', (e) => {this.checkBoxEventHandler(e)})
         return $checkBox;
     }
 
-    createLi(...items) {
-        const $taskLi = document.createElement('li');
-        for (const item of items) {
-            $taskLi.append(item);
-        }
-        return $taskLi;
+    checkBoxEventHandler(e){
+        const $checkBox = e.currentTarget;
+        const taskId = $checkBox.getAttribute('id');
+        const childNodes = $checkBox.parentNode.childNodes;
+        this.toggleLabelClass([childNodes[1], childNodes[2]]);
+        this.eventDetector('check', taskId);
     }
 
-    toggleLabelClass(labels) {
-        for (const label of labels) {
-            label.classList.toggle('label_Checked');
-        }
-    }
-
-    removeLiElement($li) {
-        $li.remove();
+    removeButtonEventHandler(element){
+        const $removeBtn = element.currentTarget;
+        const $taskLi = $removeBtn.parentNode;
+        const taskId = $taskLi.querySelector('.checkBox').getAttribute('id');
+        this.eventDetector('remove', taskId);
+        $taskLi.remove();
     }
 }
 
 class Controller {
-    constructor(dataManager, viewManager) {
+    constructor(dataManager, viewManager){
         this.dataManager = dataManager;
         this.viewManager = viewManager;
     }
 
-    init() {
-        this.setAddEvent();  
-        this.setStoredData(); 
-        this.viewManager.setEventDetector(this.viewEventDetector.bind(this)); 
+    init(){
+       viewManager.setDetector(this.viewEventDetector.bind(this));
+       this.setStoredData();
+       this.setAddEvent();
     }
 
-    viewEventDetector(eventName, event) {
-        const currentTarget = event.currentTarget;
-        const events = { 
-            check: () => { this.checkEventHandler(currentTarget); },
-            remove: () => { this.removeEventHandler(currentTarget); }
+    viewEventDetector(eventName, taskId){
+        const events = {
+            remove : () => {this.dataManager.removeTaskData(taskId)},
+            check : () => {this.dataManager.toggleTaskChecked(taskId)}
         }
         events[eventName]();
     }
 
-    checkEventHandler($checkBox) {
-        const taskId = $checkBox.getAttribute('id');
-        const childNodes = $checkBox.parentNode.childNodes; 
-        this.dataManager.toggleTaskChecked(taskId);                         
-        this.viewManager.toggleLabelClass([childNodes[1], childNodes[2]]);  
-    }
-
-    removeEventHandler($removeBtn) {
-        const $currentLi = $removeBtn.parentNode;
-        const taskId = $currentLi.querySelector('.checkBox').getAttribute('id');
-        this.dataManager.removeTaskData(taskId);        
-        this.viewManager.removeLiElement($currentLi);  
-    }
-
-    setStoredData() {
+    setStoredData(){
         const storedData = this.dataManager.getStoredData();
-        if (storedData.length === 0) return;
+        if(storedData.length === 0) return;
 
-        for (const data of storedData) {
-            this.viewManager.createNewLi(data['id'], data['contents'], data['dueDate'], data['checked']);
+        for(const data of storedData){
+            this.viewManager.addTask(data['id'], data['contents'], data['dueDate'], data['checked']);
         }
     }
 
-    setAddEvent() {
-        this.setEnterKeyAddEvent(); 
-        this.setAddButtonEvent();  
+    setAddEvent(){
+       this.setEnterKeyAddEvent();
+       this.setAddButtonEvent();
     }
 
-    setEnterKeyAddEvent() {
+    setEnterKeyAddEvent(){
         window.addEventListener('keydown', (e) => {
-            if (e.key === 'Enter') {
+            if(e.key === 'Enter'){
                 this.addButtonEventHandler();
             }
         }, true);
     }
 
-    setAddButtonEvent() {
+    setAddButtonEvent(){
         const $addButton = document.querySelector('#addBtn');
-        $addButton.addEventListener('click', (e) => {
-            this.addButtonEventHandler()
-        });
+        $addButton.addEventListener('click', (e) => {this.addButtonEventHandler()});
     }
 
-    addButtonEventHandler() {
+    addButtonEventHandler(){
         const $inputTask = document.querySelector('#taskInput');
         const $inputDate = document.querySelector('#dateInput');
         const contents = $inputTask.value;
         const dueDate = $inputDate.value;
         const taskId = this.getTaskId();
 
-        if (this.isFormEmpty(contents, dueDate)) return;
-
-        this.resetInputForm($inputTask, $inputDate);          
-        this.addTaskProcess(taskId, contents, dueDate);   
+        if(this.isFormEmpty(contents, dueDate)) return;
+        this.resetInput($inputTask, $inputDate);
+        this.addTask(taskId, contents, dueDate);
     }
 
-    getTaskId() {
+    getTaskId(){
         const timeId = new Date();
         return timeId.getTime();
     }
 
-    isFormEmpty(contents, dueDate) {
-        if (contents === "") {
+    isFormEmpty(contents, dueDate){
+        if(contents === ""){
             alert("할일을 입력해주세요!");
             return true;
         }
 
-        if (dueDate === "") {
+        if(dueDate === ""){
             alert("마감 날짜를 선택해주세요!");
             return true;
         }
@@ -236,13 +232,13 @@ class Controller {
         return false;
     }
 
-    resetInputForm($inputTask, $inputDate) {
+    resetInput($inputTask, $inputDate){
         $inputTask.value = '';
         $inputDate.value = '';
     }
 
-    addTaskProcess(taskId, contents, dueDate) {
-        this.viewManager.createNewLi(taskId, contents, dueDate);
+    addTask(taskId, contents, dueDate){
+        this.viewManager.addTask(taskId, contents, dueDate);
         this.dataManager.saveTaskData(taskId, contents, dueDate);
     }
 }
