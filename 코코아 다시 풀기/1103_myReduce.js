5075f69ca094085eed9ba0c3fddc43fdce0951c8
Hemudi <ksum1205@naver.com>
2021-12-29 00:47:51 +0900

    Feat : myReduce 함수 구현 완료
    Array.reduce() 함수와 비슷한 동작을 하는 custom reduce 함수 myReduce() 함수 구현 완료

---
+++ 코코아 다시 풀기/1103_myReduce.js
@@ -0,0 +1,22 @@
+/*
+    [ reduce 만들기 ]
+    - array 의 reduce 메서드처럼 동작하는 myReduce 만들기
+*/
+
+// arr : 입력된 배열
+// callback(reducer 함수) => (acc, cur, idx, src) => (누산기, 현재값, 현재 인덱스, 원본 배열)
+// initialValue : 초기값
+const myReduce = (arr, callback, initialValue) => {
+    let acc = initialValue ? initialValue : arr[0];
+    let idx = initialValue ? 0 : 1;
+    
+    for(;idx < arr.length; idx++){
+        acc = callback(acc, arr[idx], idx, arr);
+    }
+
+    return acc;
+}
+
+const reducer = (pre, cur) => { return pre + cur; }
+const result = myReduce([2,3,4], reducer);
+console.log(result);
