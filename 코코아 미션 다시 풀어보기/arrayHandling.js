7d868172116b6b1065a7f85ab9414a1bd49fd8ba
Hemudi <ksum1205@naver.com>
2021-12-28 23:34:37 +0900

    두번째 미션 : 배열 결과 출력 구현 완료
    json 데이터를 가져와 type 이 sk 인 객체의 name 값으로 구성된 배열 결과를 출력하는 기능 구현 완료

--- 코코아 미션 다시 풀어보기/arrayHandling.js
+++ 코코아 미션 다시 풀어보기/arrayHandling.js
@@ -1,12 +1,11 @@
-import data from './exampleData.js';
-
 /*
- - 재귀를 써서 object 가 아닐때까지 순회를 하는겨
- - 종료 조건은 object 가 아닐때
-    - 이때 값이 숫자면 결과 배열에 추가
-
+    [ 미션 1. 배열 만들기 ]
+    - value 가 object 가 아닐때까지 재귀로 순회
+        - value 가 object 가 아니면 그 값의 타입이 number 인지 체크 맞다면 결과 배열에 추가
+    - value 가 object 라면 key 순회하며 search 재귀 호출
 */
 
+import data from './exampleData.js';
 function getNumTypeValues(data) {
     const result = [];
     const search = (key, value) => {
@@ -24,3 +23,29 @@ function getNumTypeValues(data) {
 }
 
 console.log(getNumTypeValues(data));
+
+/* 
+    [ 미션 2. 배열 결과 출력 ]
+    - childnode 의 type === sk 인지 판별하고 맞다면 result 에 push
+    - childenode 의 childnode 배열을 반복문으로 돌려버리기 
+        => childnode 가 없다면 반복문 안돌아서 자동으로 종료
+*/
+
+import jsonTree from './json_tree.js';
+function getNamesOfSk(data) {
+    const result = [];
+    const search = (node) => {
+        if(node['type'] === 'sk'){
+            result.push(node['name']);
+        }
+        
+        for(const child of node['childnode']){
+            search(child);
+        }
+    }
+
+    search(data.pop());
+    return result;
+}
+
+console.log(getNamesOfSk(jsonTree));
